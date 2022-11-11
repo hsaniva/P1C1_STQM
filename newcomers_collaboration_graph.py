4a888c406effd2d2f4d045aa2774ea34d4fe2a5f
Avinash Gaikwad <avinashgdragonz@gmail.com>
2022-11-11 16:40:10 +0530

    minor modifications

--- newcomers_collaboration_graph.py
+++ newcomers_collaboration_graph.py
@@ -6,6 +6,7 @@ import numpy as np
 from bson import ObjectId
 from pylab import rcParams
 import matplotlib
+
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
@@ -18,7 +19,8 @@ commit_with_project_info = db["commit_with_project_info"]
 file_action = db["file_action"]
 file = db["file"]
 
-def createAndGetCollaborationGraph(projectName):
+
+def createAndGetCollaborationDict(projectName):
     newComers = findNewComers(projectName)
     file_devs_dict = defaultdict(set)
 
@@ -27,31 +29,29 @@ def createAndGetCollaborationGraph(projectName):
         for commit_id in commits_ids:
             file_ids = file_action.find({"commit_id": commit_id["_id"]}, {"file_id": 1})
             for file_id in file_ids:
-                file_name = file.find_one({"_id": file_id["file_id"]}, {"path":1})
+                file_name = file.find_one({"_id": file_id["file_id"]}, {"path": 1})
                 file_name = file_name["path"].split("/")[-1]
                 file_devs_dict[file_name].add(newComer)
     return file_devs_dict
 
-def beatifulGraph(parentGraph):
+
+def beatiful_graph(parentGraph):
     rcParams['figure.figsize'] = 14, 10
-    pos = nx.spring_layout(parentGraph, scale=20, k=3/np.sqrt(parentGraph.order()))
+    pos = nx.spring_layout(parentGraph, scale=20, k=3 / np.sqrt(parentGraph.order()))
     d = dict(parentGraph.degree)
     nx.draw(parentGraph, pos,
             with_labels=True,
             nodelist=d,
-            node_size=[d[k]*100 for k in d])
+            node_size=[d[k] * 100 for k in d])
     plt.show()
-def create_and_plot_graph(G):
-    parent_graph = nx.Graph()
 
+
+def create_and_graph(G):
+    parent_graph = nx.Graph()
     for fileName, devs in G.items():
         temp_graph = nx.Graph()
-        # temp_graph.add_nodes_from(list(devs))
         for dev in devs:
             temp_graph.add_node(dev)
         temp_graph.add_edges_from(itertools.combinations(list(devs), 2))
         parent_graph = nx.compose(parent_graph, temp_graph)
-    # nx.draw(parent_graph, with_labels=True, font_weight='bold')
-    # plt.show()
-    beatifulGraph(parent_graph)
-create_and_plot_graph(createAndGetCollaborationGraph("kafka"))
+    return parent_graph
