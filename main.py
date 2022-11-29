4929f98e5b869447db85240fa9be4a2b864d11b1
Avinash Gaikwad <avinashgdragonz@gmail.com>
2022-11-29 17:13:59 +0530

    added comments in the functions from the main file.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Authors: Avinash Gaikwad, Sanghmitra Tamrakar, Neha Sarnaik, Ishan Srivastava
+P1-C1
+"""
 import NewComers
 import socialHeroes
 import socioTechnical_Heros
@@ -5,27 +9,55 @@ import technical_heroes
 import technoSocialHeroes
 from newcomers_relations_with_heroes import create_and_get_social_collaboration_dict_heroes, \
     newcomers_heroes_social_graphs_relation, correlation_of_degree_of_newcomer_becoming_heroes
-from newcomers_social_collaboration_graph import create_and_get_social_collaboration_dict_newcomers
 from superHeros import findSuperHeros
 
 
+
 def get_new_comers(project_name):
+    """
+        Function for returning list of newcomers present in the project.
+        :param project_name: Project name in string
+        :return: list of newcomers present in the project
+        :rtype: list
+        """
     new_comers = NewComers.findNewComers(project_name)
     return new_comers
 
 def print_new_comers(project_name):
+    """
+    Function for printing list of newcomers present in the project.
+    :param project_name: Project name in string
+    :returns list of newcomers present in the project
+    :rtype list
+    """
     for new_comer in get_new_comers(project_name):
         print(new_comer)
 
 def get_total_devs_in_project(project_name):
+    """
+    Function for finding out the total number of developers present in the project.
+    :returns: total number of developers present in the project.
+    :rtype: integer
+    :param project_name: Project name
+    """
     print(technical_heroes.getTotalDevelopers(project_name))
 
-# get_total_devs_in_project("kafka")
 
 def get_technical_hero_devs_based_on_commit(project_name):
+    """
+    Responsible for finding out the technical developers present in the project.
+    :param project_name: Project name
+    :return: list of developers
+    :rtype: list
+    """
     return technical_heroes.findHeroDevsBasedOnCommits(project_name)
 
 def print_technical_hero_devs_based_on_commit(project_name):
+    """
+    Prints the :get_technical_hero_devs_based_on_commit list
+    :param project_name: Project name
+    :returns: none
+    """
     totalDevs = technical_heroes.getTotalDevelopers(project_name)
     heroDevsBasedOnCommit = technical_heroes.findHeroDevsBasedOnCommits(project_name)
 
@@ -36,9 +68,19 @@ def print_technical_hero_devs_based_on_commit(project_name):
     print("------------------------------------------------------------")
 
 def get_technical_hero_devs_based_on_files(project_name):
+    """
+    Function for fetching the technical heroes based on files.
+    :param project_name: Project name
+    :return: Returns list of technical developers based on the files they changed.
+
+    """
     return technical_heroes.findHeroDevsBasedOnFiles(project_name)
 
 def print_technical_hero_devs_based_on_files(project_name):
+    """
+    Responsible for printing the technical heroes based on files.
+    :param project_name: Project name
+    """
     totalDevs = technical_heroes.getTotalDevelopers(project_name)
     heroDevsBasedOnFiles = get_technical_hero_devs_based_on_files(project_name)
     print("Hero Developers : ", len(heroDevsBasedOnFiles))
@@ -48,9 +90,19 @@ def print_technical_hero_devs_based_on_files(project_name):
     print("--------------------------------------------------")
 
 def get_technical_heroes_based_on_lines(project_name):
+    """
+    Returns the technical heroes on the number of lines they committed.
+    :param project_name: Project name
+    :return: list of technical developers
+    :rtype: list
+    """
     return technical_heroes.findHeroDevsBasedOnLines(project_name)
 
 def print_technical_heroes_based_on_lines(project_name):
+    """
+    Prints the technical heroes based on the number of lines committed.
+    :param project_name: Project name
+    """
     totalDevs = technical_heroes.getTotalDevelopers(project_name)
     heroDevsBasedOnLines = get_technical_heroes_based_on_lines(project_name)
     print("Hero Developers : ", len(heroDevsBasedOnLines))
@@ -60,9 +112,18 @@ def print_technical_heroes_based_on_lines(project_name):
     print("--------------------------------------------------")
 
 def get_social_developers(project_name):
+    """
+    Returns the list of social developers
+    :param project_name: Project name
+    :return: list of developers
+    """
     return socialHeroes.findSocialHerosBasedOnComments(project_name)
 
 def print_social_developers(project_name):
+    """
+    Prints the list of social developers
+    :param project_name: Project name
+    """
     totalDevs = technical_heroes.getTotalDevelopers(project_name)
     heroDevs = get_social_developers(project_name)
     print("Hero Devs : ", len(heroDevs))
@@ -72,47 +133,103 @@ def print_social_developers(project_name):
     print("--------------------------------------------------")
 
 def get_technical_heroes(project_name):
+    """
+    Returns a list of all kinds of(intersection) technical heroes
+    :param project_name: Project name
+    :return: list of overall developers
+    """
     return technical_heroes.findOverallTechnicalDevelopers(project_name)
 
 def print_technical_heroes(project_name):
+    """
+    Prints the list of heroes received from the function :get_technical_heroes
+    :param project_name: Project name
+    """
     technical_heroes = get_technical_heroes(project_name)
     print("Total Technical Heroes : ", len(technical_heroes))
     print(technical_heroes)
     print("-------------------------------------")
 
 def get_socio_technical_heroes(project_name):
+    """
+    Returns a list of social-technical heroes present in the project.
+    :param project_name: Project name
+    :return: List of socio-technical developers
+    """
     return socioTechnical_Heros.findSocioTechnicalHeros(project_name)
 
 def print_socio_technical_heroes(project_name):
+    """
+    Prints the list of socio-technical developers received from :get_socio_technical_heroes function
+    :param project_name: Project name
+    """
     socioTechnicalHeroes = get_socio_technical_heroes(project_name)
     print("Total number of socioTechnical Heros: ", len(socioTechnicalHeroes))
     print(socioTechnicalHeroes)
 
 def get_techno_social_heroes(project_name):
+    """
+    Returns the list of techno-social heroes present in the project.
+    :param project_name: Project name
+    :return: List of techno social heroes
+    """
     return technoSocialHeroes.findTechnoSocialHeroes(project_name)
 
 def print_techno_social_heroes(project_name):
+    """
+    Prints the list of techno-social heroes present in the project.
+    :param project_name: Project name
+    """
     technoSocialheros = get_techno_social_heroes(project_name)
     print("Total number of TechnoSocial Heros: ", len(technoSocialheros))
     print(technoSocialheros)
 
 def get_super_heroes(project_name):
+    """
+    Returns the list of superheroes present in the project.
+    :param project_name: Project name
+    :return: list of heroes present in the project.
+    """
     return findSuperHeros(project_name)
 
 def print_super_heroes(project_name):
+    """
+    Prints the list received from :get_super_heroes function.
+    :param project_name: Project name
+    """
     super_heroes = get_super_heroes(project_name)
     print("Super Heroes are : ", len(super_heroes))
     print(super_heroes)
 
 def newcomer_degree_relation_due_to_heroes(project_name):
+    """
+    Returns a dictionary which specifies the newcomers
+    degree centrality change due to the introduction of heroes present in the project.
+    :param project_name: Project name
+    :return: dictionary having percentage change in degree centrality of newcomers.
+    """
     return newcomers_heroes_social_graphs_relation(project_name)
 
 def print_newcomer_degree_relation_due_to_heroes(project_name):
+    """
+    Simply prints the list of key value pairs received from :newcomer_degree_relation_due_to_heroes function.
+    :param project_name: Project name
+    """
     for key, value in newcomer_degree_relation_due_to_heroes(project_name).items():
         print(key, value)
 
 def get_correlation_of_degree_of_newcomer_becoming_heroes(project_name):
+    """
+    Correlation coefficient of newcomers becoming heroes.
+    :param project_name: Project name
+    :return: Returns Correlation coefficient for newcomers who became heroes later on.
+    """
     return correlation_of_degree_of_newcomer_becoming_heroes(project_name)
 
 def print_get_correlation_of_degree_of_newcomer_becoming_heroes(project_name):
+    """
+    Prints the correlation coefficient received from :get_correlation_of_degree_of_newcomer_becoming_heroes function.
+    :param project_name: Project name
+    """
     print("Correlation coefficient of newcomer degree ", get_correlation_of_degree_of_newcomer_becoming_heroes(project_name))
+
